c4d509a53b58623c24a3cebbb7a28bfe8360cfc9
Kosala Hemachandra <10602065+kvhnuke@users.noreply.github.com>
2020-09-23 14:05:12 -0700

    add addresses that didnt reach uniswap call level

--- accounts.js
+++ accounts.js
@@ -1,13 +1,18 @@
-const fs = require('fs');
-const addresses = require('./excludedUNIAddresses.json');
-
-console.log(addresses)
-
+const fs = require("fs");
+const Web3 = require("web3");
+const addresses = require("./excludedUNIAddresses.json");
+let excludeAddresses = require("./excludedAddresses.json");
+const FILE_NAME = "MEW/accounts.txt";
+excludeAddresses = excludeAddresses.map((address) =>
+  Web3.utils.toChecksumAddress(address)
+);
+fs.writeFileSync(FILE_NAME, "");
 for (let address of addresses) {
-    console.log(address)
-    fs.appendFile('accounts.txt', `${address}\n`, (err) => {
-        if (err) {
-            console.log(err)
-        }
-    })
+  if (excludeAddresses.indexOf(address) > -1) continue;
+  console.log(address);
+  fs.appendFile(FILE_NAME, `${address}\n`, (err) => {
+    if (err) {
+      console.log(err);
+    }
+  });
 }
